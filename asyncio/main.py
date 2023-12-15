3b08442f2a0a49b90aaa86ce99ad4270e9207da5
Andrey <125595526+AndreyPotapovAndrey@users.noreply.github.com>
2023-12-15 15:39:32 +0300

    first commit

---
+++ asyncio/main.py
@@ -0,0 +1,78 @@
+import asyncio
+import aiohttp
+from more_itertools import chunked
+from models import init_db, Session, SwapiPeople
+from pprint import pprint
+
+CHUNK_SIZE = 10  # Chank - кусочек
+
+
+# Запрос на вложенные ссылки. Возврат наименований в виде строки.
+async def internal_data(url_list, session):
+    string = []
+    for url in url_list:
+        responce = await session.get(url)
+        data = await responce.json()
+        string.append(data['name'] if 'name' in data else data['title'])
+
+    return ', '.join(string)
+
+# Получение информации о персонаже.
+async def get_person(person_id, session):
+    try:
+        url = f'https://swapi.dev/api/people/{person_id}/'
+        response = await session.get(url)
+        data = await response.json()
+
+        if response.status == 404:
+            return
+
+        # Удаление лишних полей.
+        del data['created']
+        del data['edited']
+        del data['url']
+
+        data['people_id'] = id
+        data['films'] = await internal_data(data['films'], session)
+        data['species'] = await internal_data(data['species'], session)
+        data['starships'] = await internal_data(data['starships'], session)
+        data['vehicles'] = await internal_data(data['vehicles'], session)
+
+        return data
+
+    except:
+        return
+
+
+
+async def insert_to_db(poeple_list: list):  # Принимает на вход список. + Типизируем.
+    async with Session() as session:  # Открываем сессию.
+        people = [SwapiPeople(**data) for data in poeple_list]
+        # Создаём ORM-модели с помощью list comprehension
+        # + Закидываем полученную информацию в эти модели.
+        session.add_all(people)  # Добавляем сессию.
+        await session.commit()  # Комитимся.
+
+async def main():
+    await init_db()  # Инициализируем БД.
+    session = aiohttp.ClientSession()  # Содаём сессию
+
+    for people_id_chunk in chunked(range(1, 10), CHUNK_SIZE):  # Разбиваем на последовательности по 10
+        # Без create_task мы можем перейти на следующий цикл итерации только после того,
+        # как произойдёт вставка в базу
+        coros = [get_person(person_id, session) for person_id in people_id_chunk]
+        # Создаём карутины (предподготовленные запросы)
+        result = await asyncio.gather(*coros)
+        asyncio.create_task(insert_to_db(result))  # Объект задач (create_task) формируется на базе карутин и начинает
+        # выполняться сразу после создания. Но при этом задача не блокирует дальнейшее выполнение кода.
+        # Это означает, что вставка в базу будет происходить конкурентно с получением следующей пачки персонажей.
+        # Со следующей итерацией нашего цикла.
+        pprint(result)
+
+    await session.close()
+
+    set_of_tasks = asyncio.all_tasks() - {asyncio.current_task()}  # Исключаем из сета задач main
+    await asyncio.gather(*set_of_tasks)
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ asyncio/models.py
@@ -0,0 +1,50 @@
+import os
+
+from sqlalchemy import String, Integer
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "secret")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "swapi_user")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "swapi_db")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRS_PORT = os.getenv("POSTGRES_PORT", "5431")
+
+PG_DSN = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRS_PORT}/{POSTGRES_DB}"
+
+
+engine = create_async_engine(PG_DSN)
+Session = async_sessionmaker(engine, expire_on_commit=False)  # Если expire_on_commit=True, то сессия будет  истекать
+# ("протухать") после коммита. И обязательно нужно будет создавать новую.
+
+
+class Base(DeclarativeBase, AsyncAttrs):  # Делает некоторые аттрибуты ORM-моделей (миксин) ассинхронными. К ним можно
+    # будет обращаться с помощью await
+    pass
+
+
+class SwapiPeople(Base):
+    __tablename__ = "swapi_people"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    people_id: Mapped[int] = mapped_column(Integer)
+    birth_year: Mapped[str] = mapped_column(String)
+    eye_color: Mapped[str] = mapped_column(String(20))
+    films: Mapped[str] = mapped_column(String)
+    gender: Mapped[str] = mapped_column(String(20))
+    hair_color: Mapped[str] = mapped_column(String(20))
+    height: Mapped[int] = mapped_column(Integer)
+    homeworld: Mapped[str] = mapped_column(String)
+    mass: Mapped[int] = mapped_column(Integer)
+    name: Mapped[str] = mapped_column(String(50))
+    skin_color: Mapped[str] = mapped_column(String(20))
+    species: Mapped[str] = mapped_column(String)
+    starships: Mapped[str] = mapped_column(String)
+    vehicles: Mapped[str] = mapped_column(String)
+
+
+async def init_db():
+    async with engine.begin() as conn:  # Из engine вырываем одно подключение.
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)  # Создаём все таблицы, описанные выше.
